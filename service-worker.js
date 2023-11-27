f08f7c36e610168dd9936dbe20e26433b36d4e05
shoalieh <124621042+shoalieh@users.noreply.github.com>
2023-11-27 12:42:50 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -56,7 +56,7 @@ self.addEventListener("offline", () => {
 
 // Check for online status every 5 seconds
 setInterval(() => {
-    if (navigator.onLine && isOffline) {
+    if (navigator.onLine && !isOffline) {
         isOffline = false;
         clients.matchAll({ type: "window" }).then((clients) => {
             clients.forEach((client) => {
@@ -65,7 +65,7 @@ setInterval(() => {
                 }
             });
         });
-    } else if (!navigator.onLine && !isOffline) {
+    } else if (!navigator.onLine && isOffline) {
         isOffline = true;
         clients.matchAll({ type: "window" }).then((clients) => {
             clients.forEach((client) => {
